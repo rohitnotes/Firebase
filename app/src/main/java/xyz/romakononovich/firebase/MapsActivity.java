c540d4764bfe2fbd5d6cd075b49fe848f901cbe2
Роман К <romakononovich@gmail.com>
2017-07-12 07:32:25 +0300

    Service

--- app/src/main/java/xyz/romakononovich/firebase/MapsActivity.java
+++ app/src/main/java/xyz/romakononovich/firebase/MapsActivity.java
@@ -1,9 +1,17 @@
 package xyz.romakononovich.firebase;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -12,56 +20,178 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 import xyz.romakononovich.firebase.service.LocationService;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
     private static final String TAG = MapsActivity.class.getSimpleName();
-    private static final int REQUEST_MAPS_PERMISSIONS = 5005;
+    private static final int REQUEST_MAPS_PERMISSIONS_ON_START = 5005;
+    private static final int REQUEST_MAPS_PERMISSIONS_ON_MAP_SETUP = 5006;
+    private long serviceBindTime = System.currentTimeMillis();
+    private GoogleMap googleMap;
+    private LocationService service;
+    private MyBroadcastReceiver myBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermissions(REQUEST_MAPS_PERMISSIONS_ON_START);
+        } else {
+            bindLocationService();
+        }
+        myBroadcastReceiver = new MyBroadcastReceiver();
+
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        Intent intent = new Intent(this, LocationService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LocationService.ACTION_LOCATION);
+        registerReceiver(myBroadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        unregisterReceiver(myBroadcastReceiver);
+    }
+
+    private void bindLocationService() {
+        bindService(new Intent(this, LocationService.class), connection, BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "service connected " + System.currentTimeMillis());
+
+            MapsActivity.this.service = ((LocationService.MapBinder) service).getService();
+            if (MapsActivity.this.service.currentLocation == null) {
+                Log.d(TAG, "location is null");
+            } else {
+                Log.d(TAG, MapsActivity.this.service.currentLocation.toString());
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "service disconnected");
+            MapsActivity.this.service = null;
+        }
+    };
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d(TAG, "Map ready");
-        checkPermissions();
+        Log.d(TAG, "map is ready");
+        this.googleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            checkPermissions(REQUEST_MAPS_PERMISSIONS_ON_MAP_SETUP);
+            return;
+        } else {
+            setupMap();
+        }
+
 
     }
 
-    private void checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                PackageManager.PERMISSION_GRANTED) {
-            startService(new Intent(getApplicationContext(), LocationService.class));
+    private void setupMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        googleMap.setMyLocationEnabled(true);
+    }
+
+    private void checkPermissions(int code) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            bindLocationService();
         } else {
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_MAPS_PERMISSIONS);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION}, code);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==REQUEST_MAPS_PERMISSIONS &&grantResults.length>0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_GRANTED) {
-                startService(new Intent(getApplicationContext(), LocationService.class));
+        if (requestCode == REQUEST_MAPS_PERMISSIONS_ON_START && grantResults.length > 0) {
+
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                bindLocationService();
+
+            } else {
+                new AlertDialog.Builder(this).setMessage(R.string.alert_no_gps_permission).
+                        setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).show();
+
             }
-            else {
-               new AlertDialog.Builder(this).setMessage(R.string.alert_no_gps_permission).setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                   @Override
-                   public void onClick(DialogInterface dialog, int which) {
-                       finish();
-                   }
-               }).show();
+        } else if (requestCode == REQUEST_MAPS_PERMISSIONS_ON_MAP_SETUP && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                setupMap();
+
+            } else {
+                new AlertDialog.Builder(this).setMessage(R.string.alert_no_gps_permission).
+                        setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).show();
             }
+        }
+    }
+
+    private class MyBroadcastReceiver extends BroadcastReceiver {
+        Location location;
+        LatLng myPosition;
+        CameraUpdate center;
+        CameraUpdate zoom;
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            location = MapsActivity.this.service.currentLocation;
+            myPosition = new LatLng(location.getLatitude(),
+                    location.getLongitude());
+            Log.d(TAG, myPosition.toString());
+            center = CameraUpdateFactory.newLatLng(myPosition);
+            zoom = CameraUpdateFactory.zoomTo(15);
+            googleMap.moveCamera(center);
+            googleMap.animateCamera(zoom);
         }
     }
 }
--- app/src/main/java/xyz/romakononovich/firebase/service/LocationService.java
+++ app/src/main/java/xyz/romakononovich/firebase/service/LocationService.java
@@ -2,11 +2,14 @@ package xyz.romakononovich.firebase.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -15,61 +18,88 @@ import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+import xyz.romakononovich.firebase.Manifest;
+
 /**
  * Created by romank on 29.06.17.
  */
-
-public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private Location location;
-    private GoogleApiClient mGoogleApiClient;
+public class LocationService extends Service implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private static final String TAG = LocationService.class.getSimpleName();
 
+    public Location currentLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private IBinder binder = new MapBinder();
+    public static final String ACTION_LOCATION = "action location";
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+
+        return binder;
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    public class MapBinder extends Binder {
+        public LocationService getService() {
+            return LocationService.this;
+        }
+    }
+
 
     @Override
     public void onCreate() {
         super.onCreate();
         GoogleApiClient.Builder builder = new GoogleApiClient.Builder(this);
-
-        mGoogleApiClient = builder.addConnectionCallbacks(this).
-                addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
+        mGoogleApiClient = builder.addConnectionCallbacks(this).addConnectionCallbacks(this).addApi(LocationServices.API).build();
         mGoogleApiClient.connect();
-
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Log.d(TAG, "Location Service connected");
-        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        Log.d(TAG, location.getLatitude() +" "+ location.getLongitude());
+        Log.d(TAG, "Location service connected");
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (currentLocation != null) {
+            Log.d(TAG, "initial location: " + currentLocation.getLatitude() + " " + currentLocation.getLongitude());
+            Intent intent = new Intent(ACTION_LOCATION);
+            sendBroadcast(intent);
+        }
         LocationRequest locationRequest = new LocationRequest();
         locationRequest.setInterval(5000);
         locationRequest.setFastestInterval(1000);
         locationRequest.setSmallestDisplacement(1);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,locationRequest,this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-        Log.d(TAG, "Location Service suspended");
+        Log.d(TAG, "Location service suspended");
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "Location Service failed");
+        Log.d(TAG, "Location service failed");
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        this.location = location;
-        Log.d(TAG, location.getLatitude() +" "+ location.getLongitude());
+        this.currentLocation = location;
+        if (currentLocation != null) {
+            Log.d(TAG, location.getLatitude() + " " + location.getLongitude());
+            Intent intent = new Intent(ACTION_LOCATION);
+            sendBroadcast(intent);
+        } else {
+            Log.d(TAG, "incoming location was null!");
+        }
+
     }
 }
