5cae493f3fa09d330923e1e91e803ee5ae6f6ea5
Роман К <romakononovich@gmail.com>
2017-07-19 07:40:37 +0300

    Receiving a full-screen application from the camera.

--- app/src/main/java/xyz/romakononovich/firebase/CameraActivity.java
+++ app/src/main/java/xyz/romakononovich/firebase/CameraActivity.java
@@ -2,13 +2,19 @@ package xyz.romakononovich.firebase;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,7 +23,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class CameraActivity extends AppCompatActivity {
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
     private final static String TAG = CameraActivity.class.getSimpleName();
     private static final int REQUEST_PHOTO_PERMISSIONS_CODE = 204;
     private static final int REQUEST_VIDEO_PERMISSIONS_CODE = 205;
@@ -26,6 +39,7 @@ public class CameraActivity extends AppCompatActivity {
     private ImageView imageView;
     private Button btn_left;
     private Button btn_right;
+    private String mCurrentPhotoPath;
 
 
     @Override
@@ -71,6 +85,48 @@ public class CameraActivity extends AppCompatActivity {
 
     }
 
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,
+                ".jpg"
+                ,storageDir
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void takePhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePictureIntent.resolveActivity(getPackageManager())!=null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(photoFile !=null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"xyz.romakononovich.firebase", photoFile);
+
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
+            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageURI(Uri.parse(mCurrentPhotoPath));
+
+        }
+
+    }
+
     private void requestMyPermission(int code) {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA}, code);
@@ -88,9 +144,7 @@ public class CameraActivity extends AppCompatActivity {
         int i=0;
     }
 
-    private void takePhoto() {
-        int i=0;
-    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions, @NonNull int[] grantResults) {
